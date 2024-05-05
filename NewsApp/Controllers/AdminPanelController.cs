e39f8fe4263d0f48a793acb01b8cb94e31da4162
white <pavel1712171@gmail.com>
2024-05-05 20:37:32 +0300

    Admin panel preparations update

---
+++ NewsApp/Controllers/AdminPanelController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsApp.Entities.ViewModels;
+
+namespace NewsApp.Controllers
+{
+    /// <summary>
+    /// Controller for admin panel
+    /// </summary>
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult AddUserToRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUserToRole([Bind("UserEmail, RoleId")]UserToRoleViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ NewsApp/Entities/ViewModels/UserToRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NewsApp.Entities.ViewModels
+{
+    public class UserToRoleViewModel
+    {
+        public required string UserEmail { get; set; }
+        public required string RoleId { get; set; }
+    }
+}
