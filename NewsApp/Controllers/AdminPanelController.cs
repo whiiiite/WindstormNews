27a52aa126e32c9c8a9129dd4ecb34c92c6d185b
white <pavel1712171@gmail.com>
2024-05-07 23:53:24 +0300

    Admin panel remove from role update

--- NewsApp/Controllers/AdminPanelController.cs
+++ NewsApp/Controllers/AdminPanelController.cs
@@ -1,15 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NewsApp.Data;
 using NewsApp.Entities.Models;
 using NewsApp.Entities.ViewModels;
 using NewsApp.Repositories.Users;
+using NewsApp.Shared;
+using System.Data;
 
 namespace NewsApp.Controllers
 {
     /// <summary>
     /// Controller for admin panel
     /// </summary>
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme,
+        Roles = $"{RoleNames.Owner},{RoleNames.Admin}")]
     public class AdminPanelController : Controller
     {
         private readonly NewsAppContext _context;
@@ -45,6 +51,27 @@ namespace NewsApp.Controllers
             }
 
             await _userManager.AddToRoleAsync(user, model.RoleName);
+
+            return View(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult RemoveUserFromRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveUserFromRole([Bind("UserEmail, RoleName")] UserToRoleViewModel model)
+        {
+            User? user = await _userRepository.GetUserAsync(x => x.Email == model.UserEmail);
+            if (user == null)
+            {
+                ModelState.AddModelError("user_ne", "User does not exists");
+                return View(model);
+            }
+
+            await _userManager.RemoveFromRoleAsync(user, model.RoleName);
 
             return View(nameof(Index));
         }
